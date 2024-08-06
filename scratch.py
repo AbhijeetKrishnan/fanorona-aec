9aecb80ad9497c51fd3fdb8064d03e7b08bf80cc
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2024-08-06 15:33:16 +0530

    (WIP) Attempt weight sharing

--- scratch.py
+++
@@ -1,9 +0,0 @@
-from fanorona_aec import fanorona_v1
-
-env = fanorona_v1.env()
-env.reset()
-env.step(352)
-env.render()
-print(env.state().legal_moves)
-print(env.observations['player_0']['action_mask'].nonzero())
-env.step(518)
